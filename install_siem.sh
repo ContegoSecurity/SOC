a2b7b88e5764b571cdafdc849741c19a14ff973f
ContegoSecurity <161718195+ContegoSecurity@users.noreply.github.com>
2024-03-11 15:02:59 -0300

    Update install_siem.sh
    Adicionado array de clientes e grupo siem
    check de comunicação com servidor siem

--- install_siem.sh
+++ install_siem.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
-# NA LINHA abaixo de #SIEM OS, ADICIONE O GRUPO DO CLIENTE AO LADO DE Linux <WAZUH_AGENT_GROUP='Linux,'> ex: WAZUH_AGENT_GROUP='Linux,tirol' 
-# o nome dos grupos é CASE SENSITIVE, validar nomenclatura dos grupos diretamente na interface web do Wazuh.
-# Detect the OS
+GROUP='Linux'
+#Adicione os grupos de clientes neste array. O padrão é caixa alta
+CLIENTS=("CONTEGO" "COOBRASTUR" "GEO" "LAB-CONTEGO" "TIROL")
 
-#WAZUH_GROUP="Linux, <CLIENTE>"
+#INPUT SERVIDOR SIEM
+read -p "Digite o servidor SIEM: " SIEM_SERVER
 
+if ping -c 1 "$SIEM_SERVER" >/dev/null; then
+    echo "Servidor SIEM respondeu ao ping"
+else
+    echo "O servidor $SIEM_SERVER não está acesssível. Verifique se o nome ou ip está correto. Deseja continuar? "
+    read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+fi
+
+#INPUT NOME CLIENTE
+read -p "Digite o nome do cliente: " siem_group
+
+#converte para caixa alta
+SIEM_GROUP=$(echo "$siem_group" | tr '[:lower:]' '[:upper:]')
+
+#Verifica se o nome do cliente existe no array CLIENTS
+found=0
+for client in "${CLIENTS[@]}"; do
+    if [ "$SIEM_GROUP" = "$client" ]; then
+        found=1
+        break
+    fi
+done
+
+if [ $found -eq 0 ]; then
+    echo "Cliente ${siem_group} não encontrado. Verifique se você digitou corretamente."
+    exit 1
+fi
+
+#Adiciona Linux no grupo
+SIEM_GROUPS="${GROUP},${SIEM_GROUP}"
+
+
+#Verifica OS e instala o Wazuh e Sysmon.
 OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 # Ubuntu 20.04 & 22.04
 if [[ $OS == *"Ubuntu"* ]]; then
     #SIEM Ubuntu
-    wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.0-1_amd64.deb && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' dpkg -i ./wazuh-agent_4.7.0-1_amd64.deb
+    wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.0-1_amd64.deb && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' dpkg -i ./wazuh-agent_4.7.0-1_amd64.deb
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -24,7 +57,7 @@ if [[ $OS == *"Ubuntu"* ]]; then
 # Debian 11
 elif [[ $OS == *"Debian"* ]]; then
     #SIEM Ubuntu
-    wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.0-1_amd64.deb && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' dpkg -i ./wazuh-agent_4.7.0-1_amd64.deb
+    wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.0-1_amd64.deb && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' dpkg -i ./wazuh-agent_4.7.0-1_amd64.deb
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -43,7 +76,7 @@ elif [[ $OS == *"Debian"* ]]; then
 # Fedora 36
 elif [[ $OS == *"Fedora"* ]]; then
     #SIEM RPM
-    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
+    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -55,7 +88,7 @@ elif [[ $OS == *"Fedora"* ]]; then
 # CentOS
 elif [[ $OS == *"CentOS"* ]]; then
     #SIEM RPM
-    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
+    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -67,7 +100,7 @@ elif [[ $OS == *"CentOS"* ]]; then
 # RHEL 8
 elif [[ $OS == *"Red Hat"* ]]; then
     #SIEM RPM
-    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
+    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -79,7 +112,7 @@ elif [[ $OS == *"Red Hat"* ]]; then
 # RHEL 9
 elif [[ $OS == *"Red Hat"* ]]; then
     #SIEM RPM
-    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
+    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
@@ -91,7 +124,7 @@ elif [[ $OS == *"Red Hat"* ]]; then
 # openSUSE 15
 elif [[ $OS == *"openSUSE"* ]]; then
     #SIEM RPM
-    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='siem.contego.com.br' WAZUH_AGENT_GROUP='Linux' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
+    curl -o wazuh-agent-4.7.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.0-1.x86_64.rpm && sudo WAZUH_MANAGER='${SIEM_SERVER}' WAZUH_AGENT_GROUP='${SIEM_GROUPS}' rpm -ihv wazuh-agent-4.7.0-1.x86_64.rpm
     sudo systemctl daemon-reload
     sudo systemctl enable wazuh-agent
     sudo systemctl start wazuh-agent
